dd18d06e6c0e90b6545b87a46b11ae9e210402ba
lear <1325925707@qq.com>
2019-06-14 13:04:26 +0800

    fix: 添加错误处理catch

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const notifier = require('node-notifier');
+
 const App = require('./src');
 
-new App().start();
+new App().start().catch(e => {
+  notifier.notify({
+    title: 'LEARN',
+    message: e.message || e
+  });
+  process.exit(0);
+});
