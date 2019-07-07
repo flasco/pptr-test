c5700c5e15925b26eb2bb04ae35e671dbb72f738
lear <1325925707@qq.com>
2019-07-07 16:01:47 +0800

    fix: add sth intrested

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const { time: Logger } = require('@flasco/logger');
+
 const notifier = require('node-notifier');
 
 const App = require('./dist');
+
+Logger.info('welcome to use');
+Logger.info('current version:', process.env.npm_config_init_version);
 
 new App().start().catch(e => {
   console.log(e);
   notifier.notify({
     title: 'LEARN',
-    message: e.message || e
+    message: e.message || e,
   });
   process.exit(0);
 });
