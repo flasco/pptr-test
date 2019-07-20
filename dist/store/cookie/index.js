13a6b41610580e91983e6796de24e6b0461bb079
lear <1325925707@qq.com>
2019-07-20 21:47:33 +0800

    v1.2.0

--- dist/store/cookie/index.js
+++ dist/store/cookie/index.js
@@ -1,16 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
+const logger_1 = require("@flasco/logger");
 const base_1 = tslib_1.__importDefault(require("../base"));
 class Cookie extends base_1.default {
     constructor() {
-        super('cookie', { str: '' });
+        super('cookie', { str: '', time: 0 });
     }
     getCookie() {
-        return this.getStore().str;
+        const store = this.getStore();
+        const time = (store.time || 0);
+        if (Date.now() - time > 3600000 * 3) {
+            logger_1.time.info('cookie 存储时长超过三小时，请重新登录');
+            return '';
+        }
+        return store.str;
     }
     setCookie(str) {
         this.store.str = str;
+        this.store.time = Date.now();
         this.setStore(this.store);
         this.save();
     }
--- src/store/cookie/index.ts
+++ src/store/cookie/index.ts
@@ -1,15 +1,23 @@
+import { time as Logger} from '@flasco/logger';
 import Base from '../base';
 
 class Cookie extends Base {
   constructor() {
-    super('cookie', { str: '' });
+    super('cookie', { str: '', time: 0 });
   }
   getCookie() {
-    return this.getStore().str;
+    const store = this.getStore();
+    const time = <number>(store.time || 0);
+    if (Date.now() - time > 3600000 * 3) {
+      Logger.info('cookie 存储时长超过三小时，请重新登录');
+      return '';
+    }
+    return store.str;
   }
 
   setCookie(str: string) {
     this.store.str = str;
+    this.store.time = Date.now();
     this.setStore(this.store);
     this.save();
   }
